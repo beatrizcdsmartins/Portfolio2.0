5653142e5299e9a6254a7d1dc671c87b6b801b69
beatrizcdsmartins <beatrizcdsmartins@gmail.com>
2024-07-03 17:27:26 -0300

    add sark animation

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ const hideDiv = document.getElementById('hide-content')
 ButtonSeeMore.addEventListener('click', () => {
     hideDiv.classList.toggle('show');
     if (hideDiv.classList.contains('show')) {
-        ButtonSeeMore.innerHTML = 'Veja menos';
+        ButtonSeeMore.innerHTML = 'Ver menos';
         ButtonSeeMore.style.backgroundColor = '#D62828';
     } else {
-        ButtonSeeMore.innerHTML = 'Veja mais';
+        ButtonSeeMore.innerHTML = 'Ver mais';
         ButtonSeeMore.style.backgroundColor = '#FCBF49';
     }
 });
@@ -24,6 +24,32 @@ document.addEventListener('DOMContentLoaded', function () {
 }); // menu-hamburguer
 
 
+// Seletores
+const lamp = document.querySelector('.lamp');
+const containerLamp = document.querySelector('.cta');
+const textLamps = document.querySelectorAll('.txt-cta');
+const switches = document.querySelectorAll('.switch');
 
+// Função para alternar a lâmpada
+function toggleLamp() {
+    containerLamp.classList.toggle('active');
+    textLamps.forEach(textLamp => {
+        textLamp.classList.toggle('active');
+    });
 
+    if (containerLamp.classList.contains('active')) {
+        lamp.src = './assets/icons/dark-lamp.svg';
+    } else {
+        lamp.src = './assets/icons/light_lamp.svg'; 
+    }
+}
+lamp.addEventListener('click', toggleLamp);
+
+// Evento de clique nos switches
+switches.forEach(switchElement => {
+    switchElement.addEventListener('click', function() {
+        document.documentElement.classList.toggle('dark-mode');
+        toggleLamp(); // Chama a função para alternar a lâmpada
+    });
+});
 
